2d745be38015d14f4d7ce0bf4a826e0cd8dfeb82
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-27 20:58:35 +0200

    setup inital form structure and submit

---
+++ src/components/Form.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const Form = () => {
+    const [newItemName, setNewItemName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(e.target.value === '') {
+          console.log('empty input')
+          return
+        }
+
+        console.log(newItemName);
+      }
+
+  return (
+    <section className='section-center'>
+      <form onSubmit={handleSubmit}>
+        <h4>Grocery Buddy</h4>
+        <div className='form-control'>
+            <input type='text' 
+            className='form-input' 
+            value={newItemName}
+            onChange={(e) => setNewItemName(e.target.value)} />
+        </div>
+       
+        <button type='submit' className='btn'>Add Item</button>
+      </form>
+    </section>
+  )
+}
+export default Form
